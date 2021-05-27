99278bf2ef5c0b4a1582ad190ac80564744e4d1a
Aman <amangn12@gmail.com>
2021-05-27 21:26:47 +0530

    third commit

--- EmployeeeManagement/Controllers/AdministrationController.cs
+++ EmployeeeManagement/Controllers/AdministrationController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EmployeeeManagement.ViewModel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeeManagement.Controllers
 {
+    [Authorize(Roles="Admin")]
     public class AdministrationController : Controller
     {
         private readonly UserManager<IdentityUser> userManager;
